55cb56228a168e21568435b40aafe2ffbb6f3db7
István Kiss <ikiss@chemaxon.com>
2021-03-10 12:56:02 +0100

    Spring init

---
+++ src/main/java/com/example/optaplanner/OptaplannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.optaplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptaplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptaplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/optaplanner/OptaplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optaplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptaplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
